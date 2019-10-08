2bc163aa95e0bf6792c12eaec2a1a9c886cbb439
Alexander Yaroslavtsev <a1exweb@mail.ru>
2019-10-08 12:40:05 +0300

    слайдер owl

---
+++ lesson-19-extra/src/js/portfolio-slider.js
@@ -0,0 +1,19 @@
+$(function(){
+  $('.slider').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      navText : ['<img src="img/portfolio/left-arrow.png" alt="Предыдущий">','<img src="img/portfolio/right-arrow.png" alt="Следующий">'],
+      responsive:{
+          0:{
+              items:1
+          },
+          768:{
+              items:2
+          },
+          1200:{
+              items:3
+          }
+      }
+  });
+});
