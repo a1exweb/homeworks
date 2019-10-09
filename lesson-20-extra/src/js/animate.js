8b74e117c1dee289c245b608675763d77be0e0fe
Alexander Yaroslavtsev <a1exweb@mail.ru>
2019-10-09 19:19:11 +0300

    fix structure

--- lesson-20-extra/src/js/animate.js
+++ lesson-20-extra/src/js/animate.js
@@ -1,29 +1,28 @@
 let animateHTML = function() {
-  let elems;
-  let windowHeight;
-  function init() {
-      elems = document.querySelectorAll('.block_anim');
-      windowHeight = window.innerHeight;
-      addEventHandlers();
-      checkPosition();
-  };
-  function addEventHandlers() {
-      window.addEventListener('scroll', checkPosition);
-      window.addEventListener('resize', init);
-  };
-  function checkPosition() {
-      for (let i = 0; i < elems.length; i++) {
-      let positionFromTop = elems[i].getBoundingClientRect().top;
-          if (positionFromTop - windowHeight <= 0) {
-              elems[i].className = elems[i].className.replace(
-              'block_anim',
-              'fadeInLeftBig'
-              );
-          }
-      }
-  }
-  return {
-      init: init
-  };
- };
- animateHTML().init();
+    let elems = document.querySelectorAll('.block_anim');
+    let windowHeight = window.innerHeight;
+
+    function addEventHandlers() {
+        window.addEventListener('scroll', checkPosition);
+        window.addEventListener('resize', init);
+    };
+    function checkPosition() {
+        for (let i = 0; i < elems.length; i++) {
+        let positionFromTop = elems[i].getBoundingClientRect().top;
+            if (positionFromTop - windowHeight <= 0) {
+                elems[i].className = elems[i].className.replace(
+                'block_anim',
+                'fadeInLeftBig'
+                );
+            }
+        }
+    }
+    function init() {
+    addEventHandlers();
+    checkPosition();
+    };
+    return {
+        init: init
+    };
+};
+animateHTML().init();
